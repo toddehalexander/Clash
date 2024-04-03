4a9cff2655d78a0017fc1c0274b7ae89ab035b4a
Toddeh <alexandertoddeh@gmail.com>
2024-04-03 00:45:56 -0700

    updated responses

--- discord/responses.py
+++ discord/responses.py
@@ -5,11 +5,11 @@ def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
     if lowered == '':
-        return 'Well, you\'re awfully silent...'
+        return 'Well, you\'re awfully silent...' 
     elif 'hello' in lowered:
         return 'Hello there!'
     elif 'how are you' in lowered:
-        return 'Good, thanks!'
+        return 'Good, thanks !'
     elif 'bye' in lowered:
         return 'See you!'
     elif 'roll dice' in lowered:
